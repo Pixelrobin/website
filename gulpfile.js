31d244e9c5ac87932414dc34f9b385f703b4c664
Michael Savchuk <pixelrobin@hotmail.com>
2018-12-03 01:03:14 -0800

    New build system

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass    = require('gulp-sass');
 const pug     = require('gulp-pug');
 const bs      = require('browser-sync');
 const version = require('gulp-version-number');
+const newer   = require('gulp-newer');
+const del     = require('del');
 
 gulp.task('styles', done =>
 	gulp.src('src/styles/*.scss')
@@ -23,16 +25,35 @@ gulp.task('views', done =>
 				'to': ['css', 'js'],
 			}
 		}))
-		.pipe(gulp.dest('./'))
-		//.pipe(bs.reload())
+		.pipe(gulp.dest('dist/'))
 );
 
-gulp.task('dev', gulp.parallel('styles', 'views', done => {
+gulp.task('media', done =>
+	gulp.src('media/**/*')
+		.pipe(newer('dist/media/'))
+		.pipe(gulp.dest('dist/media'))
+);
+
+gulp.task('root', done =>
+	gulp.src('src/root/**/*')
+		.pipe(newer('dist/'))
+		.pipe(gulp.dest('dist/'))
+);
+
+gulp.task('clean', done =>
+	del('dist/**/*')
+);
+
+gulp.task('dev', gulp.parallel('styles', 'views', 'media', 'root', done => {
 	bs.init({
-		server: { baseDir: './', },
+		server: { baseDir: './dist/', },
 		port: 3000
 	});
 
 	gulp.watch('src/styles/**/*.scss', gulp.parallel('styles'));
 	gulp.watch('src/views/**/*.pug', gulp.parallel('views')).on('change', bs.reload);
+	gulp.watch('src/root/**/*', gulp.parallel('root')).on('change', bs.reload);
+	gulp.watch('media/**/*', gulp.parallel('media')).on('change', bs.reload);
 }));
+
+gulp.task('build', gulp.parallel('clean', 'styles', 'views', 'media', 'root'));
