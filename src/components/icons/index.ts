4b9ea4c3d33f5b20a4010adc31ac57428275abe5
Michael Savchuk <michael@solegraphics.com>
2024-10-26 09:59:13 -0300

    Add social icon linnks to homepage

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Bluesky } from './Bluesky.astro';
+export { default as GitHub } from './GitHub.astro';
+export { default as ItchIo } from './ItchIo.astro';
+export { default as LinkedIn } from './LinkedIn.astro';
